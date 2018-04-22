7ba046232b316c724e33cf5f1f9aec9e3a99d7b1
Raghu K <raghuk.vit@gmail.com>
2018-04-22 01:18:57 +0000

    Updating setup-scripts

---
+++ setup-scripts/setup-master.sh
@@ -0,0 +1,1 @@
+curl -s https://raw.githubusercontent.com/linuxautomations/kubernetes/master/setup-master.sh | bash
---
+++ setup-scripts/setup-node.sh
@@ -0,0 +1,1 @@
+curl -s https://raw.githubusercontent.com/linuxautomations/kubernetes/master/setup-node.sh | bash
